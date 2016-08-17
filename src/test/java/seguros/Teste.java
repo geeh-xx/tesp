75d06a8aa5ace8cde09b27ae89ae3053909d7065
Geeh <rangel.soares.souza@gmail.com>
2016-08-17 14:49:19 -0300

    exercicio 1

--- src/test/java/seguros/Teste.java
+++ src/test/java/seguros/Teste.java
@@ -1,19 +1,46 @@
 package seguros;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.unibh.seguros.entidades.Funcionario;
 import br.unibh.seguros.entidades.Segurado;
+import br.unibh.seguros.entidades.Setor;
 
 public class Teste {
 	
+	Funcionario funcionario = new Funcionario("RH","João","joao","12345");
+
+	Segurado segurado = new Segurado("Teste", "12345", "SSP-MG","12345", "B",
+			new Date(),new Date(), "Rua", "meca", "195", 
+			"30590390","betania", "BH", "MG");
+	
+	Setor setor = new Setor(new Long(1), "Recursos Humanos", "RH");
+	
 	@Test
 	public void criarObjetos(){
-		Funcionario funcionario = new Funcionario("RH","João","joao","12345");
 		Assert.assertEquals("João",funcionario.getPerfil());
 		
-		//Segurado segurado = new Segurado("B", "111111", "SSP-MG", "1111111", "AB", "11/2022", dataPrimeiraHabilitacao, logradouro, numero, complemento, cep, bairro, cidade, estado)
+		Assert.assertEquals("12345",segurado.getNumeroRG());
+
+		Assert.assertEquals("RH",setor.getSigla());
+		
 	}
+	
+	@Test
+	public void comparaObjetos(){
+
+		Segurado segurado2 = segurado;
+		Funcionario funcionario2 = funcionario;
+		Setor setor2 = setor;
+		
+		Assert.assertEquals(segurado, segurado2);
+		Assert.assertEquals(funcionario, funcionario2);
+		Assert.assertEquals(setor, setor2);		
+		
+	}
+	
 
 }
